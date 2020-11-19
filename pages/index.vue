b710690da928e3aff0d5123fac10384ffb32d1b3
Simon COTELLE <Simon.COTELLE@diiage.org>
2020-11-19 10:06:21 +0100

    FEAT(index)

--- pages/index.vue
+++ pages/index.vue
@@ -7,10 +7,9 @@
       <div>
         <h1 class="title">Choose your dream destination...</h1>
         <div class="links">
-          <a href="#" class="giant-button"> San Francisco, USA </a>
-          <a href="#" class="giant-button"> Québec, Canada </a>
-          <a href="#" class="giant-button"> Bora Bora, Polynésie </a>
-          <a href="#" class="giant-button"> Torres del Paine, Chile </a>
+          <a v-for="(destination, index) in destinations" :key="index" href="#" class="giant-button">
+            {{ destination.name }}
+          </a>
         </div>
       </div>
     </div>
@@ -18,51 +17,68 @@
 </template>
 
 <script>
-export default {}
+  import {
+    mapGetters
+  } from 'vuex'
+  export default {
+    async asyncData({
+      store
+    }) {
+      await store.dispatch('destinations/loadDestinations');
+    },
+    computed: {
+      ...mapGetters({
+        destinations: 'destinations/getDreamDestinations',
+      }),
+    },
+  }
+
 </script>
 
 <style lang="scss">
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-}
-
-.toolbar {
-  height: 80px;
-  background-color: #35495e;
-  align-self: flex-start;
-  width: 100%;
-}
-
-.content {
-  display: flex;
-  flex-direction: column;
-  flex: 1;
-  align-items: center;
-  justify-content: center;
-  background-image: url('~assets/airplane.jpg');
-  background-size: cover;
-}
-
-.title {
-  font-weight: 500;
-  font-size: 58px;
-  color: #fff;
-  letter-spacing: 1px;
-  text-shadow: 1px 1px 4px #000;
-  text-align: center;
-}
-
-.links {
-  display: flex;
-  flex-flow: row wrap;
-  align-items: center;
-  justify-content: center;
-  padding-top: 15px;
-  & > * {
-    margin: 5px;
+  .container {
+    margin: 0 auto;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
   }
-}
+
+  .toolbar {
+    height: 80px;
+    background-color: #35495e;
+    align-self: flex-start;
+    width: 100%;
+  }
+
+  .content {
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+    background-image: url('~assets/airplane.jpg');
+    background-size: cover;
+  }
+
+  .title {
+    font-weight: 500;
+    font-size: 58px;
+    color: #fff;
+    letter-spacing: 1px;
+    text-shadow: 1px 1px 4px #000;
+    text-align: center;
+  }
+
+  .links {
+    display: flex;
+    flex-flow: row wrap;
+    align-items: center;
+    justify-content: center;
+    padding-top: 15px;
+
+    &>* {
+      margin: 5px;
+    }
+  }
+
 </style>
---
+++ store/destinations/actions.js
@@ -0,0 +1,14 @@
+import service from './service'
+
+export default {
+  async loadDestinations({
+    commit
+  }) {
+    try {
+      const destinations = await service.getAll();
+      commit('setDestinations', destinations);
+    } catch (e) {
+      console.error(e);
+    }
+  },
+}
